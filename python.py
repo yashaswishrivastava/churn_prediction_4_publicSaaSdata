cbb5d23fd19b13a064c4e107499019e02e68374e
Yashaswi Shrivastava <79009595+yashaswishrivastava@users.noreply.github.com>
2026-05-11 11:24:38 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,19 @@
+import pandas
+import numpy
+import sklearn
+import xgboost
+import lightgbm
+import imblearn
+import shap
+import mlflow
+import fastapi
+
+print("All packages loaded successfully")
+print(f"Python: 3.13.3")
+print(f"pandas:      {pandas.__version__}")
+print(f"numpy:       {numpy.__version__}")
+print(f"sklearn:     {sklearn.__version__}")
+print(f"xgboost:     {xgboost.__version__}")
+print(f"lightgbm:    {lightgbm.__version__}")
+print(f"shap:        {shap.__version__}")
+print(f"mlflow:      {mlflow.__version__}")
